5d606481bbee2776b143656fc434ace89c13a9ab
Chadwick <83472455+productivechad@users.noreply.github.com>
2021-09-29 13:48:38 -0400

    Add files via upload

---
+++ ACME Accounting/ACME Accounting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ACME_Accounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting” on you! ");
+            Console.ReadLine();
+        }
+    }
+}
