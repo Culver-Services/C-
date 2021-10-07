73713efc7a0cfda142007da735c41b088be50a6b
Chadwick Culver <83472455+productivechad@users.noreply.github.com>
2021-10-07 15:26:31 -0400

    Add files via upload

---
+++ String_AssignSub/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_AssignSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Chadwick";
+            string hobby = "coding";
+            string location = "home";
+
+            Console.WriteLine(name + " likes to do " + hobby + " from " + location + "!");
+
+            Console.WriteLine(name.ToUpper());
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("This is a paragraph of text.");
+            sb.Append("\nIt is made one sentence at a time.");
+            sb.Append("\nI am not sure what it's about.");
+            sb.Append("\nRegardless, here it is!");
+
+            Console.WriteLine(sb);
+
+            Console.ReadLine();
+        }
+    }
+}
