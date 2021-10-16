f490603aa6940f1e5ff3898359d1ab7744a10e13
Chadwick Culver <83472455+productivechad@users.noreply.github.com>
2021-10-15 20:01:41 -0400

    Add files via upload

---
+++ Classes and Methods App/MathFunction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_App
+{
+    public class MathFunction
+    {
+        public int mathOne(int num1)
+        {
+            int num2 = 7;
+            var Total = num1 * num2;
+            return Total;
+        }
+
+        public int mathOne(decimal num1)
+        {
+            decimal num2 = 3.06m;
+            var answer = Convert.ToInt32(num1 / num2);
+            return answer;
+        }
+
+        public int mathOne(string stringOne)
+        {
+            int num3 = 23;
+            int num4 = Convert.ToInt32(stringOne);
+            int conclusion = num4 - num3;
+            return conclusion;
+        }
+    }
+}
---
+++ Classes and Methods App/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Classes_and_Methods_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int z = 1;
+            int Total;
+            MathFunction functionOne = new MathFunction();
+
+            Total = functionOne.mathOne(z);
+
+            Console.WriteLine("Multiply 7 * 1");
+            Console.WriteLine(Total);
+
+            decimal q = 81.81m;
+            int answer;
+            MathFunction functionTwo = new MathFunction();
+            answer = functionTwo.mathOne(q);
+
+            Console.WriteLine("Divide 81.81 by 3.06");
+            Console.WriteLine(answer);
+
+
+            string x;
+            int conclusion;
+            MathFunction functionThree = new MathFunction();
+            Console.WriteLine("Enter a number for the final Method:");
+            x = (Console.ReadLine());
+            Console.WriteLine(x + " minus 23 equals:");
+            conclusion = functionThree.mathOne(x);
+
+            Console.WriteLine(conclusion);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
