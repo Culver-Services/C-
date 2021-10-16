83f34669cf7cec3290806c409b8d456a3fb4d907
Chadwick Culver <83472455+productivechad@users.noreply.github.com>
2021-10-15 21:00:53 -0400

    Add files via upload

---
+++ ClassMethodApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMethodApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int answer;
+            MathFunctions functionOne = new MathFunctions();
+
+            Console.WriteLine("Please enter a number.");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+            functionOne.divided(num1, out answer);
+            Console.WriteLine(num1 + " divided by 2 equals:" + answer);
+
+
+            int num2;
+            int num3;
+
+            MathFunctions functionTwo = new MathFunctions();
+            functionTwo.numbers(out num2, out num3);
+            Console.WriteLine("");
+
+            int int1 = 19;
+            int int2 = 12;
+            int total;
+            MathFunctions functionThree = new MathFunctions();
+            functionThree.numbers(int1, int2, out total);
+            Console.WriteLine(int1 + " plus " + int2 + " equals " + total);
+            Console.ReadLine();
+        }
+    }
+}
