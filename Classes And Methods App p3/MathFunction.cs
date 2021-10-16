0faae2ffc361d579942e3eae5bc0992395865cb5
Chadwick Culver <83472455+productivechad@users.noreply.github.com>
2021-10-15 20:26:12 -0400

    Add files via upload

---
+++ Classes And Methods App p3/MathFunction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_And_Methods_App_p3
+{
+    class MathFunction
+    {
+        public void functionOne(int int1, int int2)
+        {
+            int Total = int1 * 4;
+            Console.WriteLine(int2);
+        }
+    }
+}
+
---
+++ Classes And Methods App p3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes_And_Methods_App_p3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 7;
+            int num2 = 3;
+            MathFunction function = new MathFunction();
+
+            function.functionOne(num1, num2);
+
+            function.functionOne(int1: num1, int2: num2);
+
+            Console.ReadLine();
+        }
+    }
+}
