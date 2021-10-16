3917e983db256dd31919482db06877af798b811b
Chadwick Culver <83472455+productivechad@users.noreply.github.com>
2021-10-15 20:15:36 -0400

    Add files via upload

---
+++ Classes and Methods App 2/MathFunction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_App_2
+{
+    class MathFunction
+    {
+        public int twoInt(int int1, int int2 = 0)
+        {
+            int Total;
+            Total = int1 * 1 + int2;
+            return Total;
+        }
+    }
+}
