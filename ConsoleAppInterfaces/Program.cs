854f12acb37a1d1b60b37f59bd4add92b089f991
Chadwick Culver <83472455+productivechad@users.noreply.github.com>
2021-10-17 22:40:38 -0400

    Add files via upload

---
+++ ConsoleAppInterfaces/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable employeeTwo = new Employee();
+            employeeTwo.Quit();
+            Console.ReadLine();
+        }
+    }
+}
