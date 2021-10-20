d47b6b5485d0d56f74bd7f870b7c0d2d9c54b8b1
Chadwick Culver <83472455+productivechad@users.noreply.github.com>
2021-10-20 14:39:35 -0400

    Add files via upload

---
+++ Additional Features Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string businessName = "Culver.co";
+            var greeting = "Welcome to " + businessName;
+            Console.WriteLine(greeting);
+            Employee chadwick = new Employee("Chadwick");
+            Console.WriteLine("{0}, You are employee number:{1}", chadwick.empName, chadwick.empId);
+            Console.ReadLine();
+        }
+    }
+}
