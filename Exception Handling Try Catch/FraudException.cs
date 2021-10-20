ff990b48ec27c270bc1a80c55b49972b10b6ab5a
Chadwick Culver <83472455+productivechad@users.noreply.github.com>
2021-10-20 15:00:26 -0400

    Add files via upload

---
+++ Exception Handling Try Catch/FraudException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling_Try_Catch
+{
+    class FraudException : Exception
+    {
+        public FraudException()
+           : base() { }
+        public FraudException(string message)
+            : base(message) { }
+    }
+}
